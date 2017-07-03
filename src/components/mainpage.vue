4b85260553146c6a38c9ff7d21894d0a91aa3b59
barkermen <barkermen1996@gmail.com>
2017-07-03 16:49:03 +0300

    Создана главная страница Вопросника issue #2

---
+++ src/components/mainpage.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="mainpage">
+    <h1>{{ msg }}</h1>
+   <p>
+  <button type="button" class="btn btn-default btn-lg">Большая кнопка</button>
+</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'mainpage',
+  data () {
+    return {
+      msg: 'Anonimus fask'
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
