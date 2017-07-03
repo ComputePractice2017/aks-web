2a2c12acb318a4f63683bd1c928b20a66abec084
barkermen <barkermen1996@gmail.com>
2017-07-03 18:38:22 +0300

    досоздал главную страницу issue #2

--- src/components/mainpage.vue
+++ src/components/mainpage.vue
@@ -2,8 +2,18 @@
   <div class="mainpage">
     <h1>{{ msg }}</h1>
    <p>
-  <button type="button" class="btn btn-default btn-lg">Большая кнопка</button>
+  <button type="button" class="btn btn-default btn-lg">Жмак</button>
 </p>
+<div class="form-group">
+    <label for="vopros">Здесь будет дана ссылка на задание вопроса и на полный список вопросов</label>
+    <input type="vopros" class="form-control" id="vopros" aria-describedby="vopros" placeholder="Ссылка на вопрос">
+    
+  </div>
+  <div class="form-group">  
+    <label for="otvet">Здесь будет дана ссылка для ответа на вопрос</label>
+    <input type="otvet" class="form-control" id="otvet" aria-describedby="otvet" placeholder="Ссылка на ответ">
+    
+  </div>
   </div>
 </template>
 
